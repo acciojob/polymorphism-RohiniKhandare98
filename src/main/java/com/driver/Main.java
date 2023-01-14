050e077c2c48fe45db91eba59522db2ab81aeafa
unknown <rohini.khandare16598@gmail.com>
2023-01-14 19:48:56 +0530

    updated polymorphism solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,15 +8,15 @@ public class Main {
         p.product(2.2 ,3.2);
 
     }
-        static class Product{
+       public static class Product{
             public int product(int x , int y){
-                return x+y;
+                return x*y;
             }
             public int product(int x , int y ,int z){
-                return x+y+z;
+                return x*y*z;
             }
             public double product(double x ,double y){
-                return x+y;
+                return x*y;
             }
 
         }
